b28bd20e79a5316d004fb64da22ee3b04fe118bd
aloramercer <beyzakomis@gmail.com>
2024-08-16 16:26:16 +0300

    Created a styles file for styling

---
+++ src/styles.js
@@ -0,0 +1,28 @@
+
+import { imageListClasses } from '@mui/material';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: '#000',
+  },
+  input: {
+    width: '80%', // Adjust width as needed
+    marginBottom: 16, // Add margin between inputs
+  },
+  button: {
+    marginTop: 16, // Add margin above the button
+  },
+  image:{
+    margin:30,
+
+  }
+});
+
+export default styles;
