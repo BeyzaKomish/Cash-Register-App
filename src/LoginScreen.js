6d78d79f11135a3f3bb277019b2d67bc559116f7
aloramercer <beyzakomis@gmail.com>
2024-08-16 16:22:13 +0300

    Created a basic login page with some design elements

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { TextInput , Button , Avatar} from 'react-native-paper';
+import styles from './styles';
+
 
 
 
 function LoginScreen(){
     return(
         <View style={{  flex: 1,
-            backgroundColor: '#fff',
             alignItems: 'center',
             justifyContent: 'center'
             
             }}>
 
-        <Text>Open up App.js to start working on your app!</Text>
+            <Avatar.Image style={styles.image} size={180} source={require('../assets/images/toyota-market-logo.png')} />
+        <TextInput style={styles.input} label="User-Code" mode="outlined" />
+      <TextInput style={styles.input} label="Password" mode="outlined" secureTextEntry />
+      <Button style={styles.button}   mode="contained">LOGIN</Button>
         <StatusBar style="auto" />
       </View>
 
