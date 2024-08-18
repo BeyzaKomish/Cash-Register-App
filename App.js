cc9ebf80c06917a42189cb77b7b0ac9a7267a596
aloramercer <beyzakomis@gmail.com>
2024-08-18 14:57:31 +0300

    Imported and used  Toast from  react-native-toast message for alert message box when user leaves the inputs empty in the login page

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import LoginScreen from './src/LoginScreen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
+import LoginScreen from './src/screens/LoginScreen'
+import HomeScreen from './src/screens/HomeScreen'
+import 'react-native-gesture-handler'
+import Toast from 'react-native-toast-message';
 
-
-import { StyleSheet, Text, View } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LoginPage" component={LoginScreen}/>
+        <Stack.Screen name="Please Login" component={LoginScreen}/>
+
       </Stack.Navigator>
+      <Toast  
+      />
     </NavigationContainer>
 
   );
 }
 
-
+//i was plannning on using a json server to mock services but is this possible :Login Authentication Tutorial in React Native using AsyncStorage | useReducer, useContext, useMemo
