b21e22e146e89c08bca7f53d0f06728c76a7c99e
aloramercer <beyzakomis@gmail.com>
2024-08-25 16:50:51 +0300

    "Adjusted Stack Navigator"

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen'
+import LoginScreen from './src/screens/LoginScreen.js'
 import HomeScreen from './src/screens/HomeScreen'
 import 'react-native-gesture-handler'
 import Toast from 'react-native-toast-message';
@@ -10,15 +10,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Please Login" component={LoginScreen}/>
-
-      </Stack.Navigator>
-      <Toast  
-      />
-    </NavigationContainer>
-
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="LoginScreen">
+              <Stack.Screen name="LoginScreen" component={LoginScreen} />
+              <Stack.Screen name="HomeScreen" component={HomeScreen} />
+             
+          </Stack.Navigator>
+          <Toast/>
+      </NavigationContainer>
   );
 }
 
