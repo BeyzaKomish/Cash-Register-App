6908922f14698046ca28f63302bb76f24c4ef9c1
aloramercer <beyzakomis@gmail.com>
2024-08-25 16:56:53 +0300

    "Created New Screens For Further Use"

---
+++ src/screens/SalesScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from '../styles';
+export default function SalesScreen() {
+  return (
+    <View>
+      <Text>SalesScreen</Text>
+    </View>
+  )
+}
